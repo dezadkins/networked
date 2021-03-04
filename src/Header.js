34526170e7043e6b4925f7b7c77f89b48b34845b
Dez <dez@dezs-mbp.attlocal.net>
2021-03-04 09:49:04 -0500

    all icons in

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import HeaderOption from "./components/HeaderOption/HeaderOption";
 import HomeIcon from "@material-ui/icons/Home";
 import GroupIcon from "@material-ui/icons/Group";
-
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 function Header() {
   return (
     <div className="header">
@@ -22,6 +24,9 @@ function Header() {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={GroupIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
       </div>
     </div>
   );
--- src/components/HeaderOption/HeaderOption.js
+++ src/components/HeaderOption/HeaderOption.js
@@ -5,7 +5,6 @@ function HeaderOption({ Icon, title }) {
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
-
       <h3 className="headerOption__title">{title}</h3>
     </div>
   );
