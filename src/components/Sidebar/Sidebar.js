f266056eab90621f19f3ba8d8a932b027c4fc6c7
Dez <dez@dezs-mbp.attlocal.net>
2021-03-11 14:35:25 -0500

    sidebar recent hashtags

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,6 +3,13 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
@@ -28,6 +35,11 @@ function Sidebar() {
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("developer")}
+        {recentItem("design")}
       </div>
     </div>
   );
