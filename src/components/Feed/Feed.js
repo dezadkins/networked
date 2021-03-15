98d3d9f59abcef52efd0ac25689749bcd1071678
Dez <dez@dezs-mbp.attlocal.net>
2021-03-15 11:46:50 -0400

    input field setup

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "../InputOption/InputOption";
+import PanoramaOutlinedIcon from "@material-ui/icons/PanoramaOutlined";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import PostAddOutlinedIcon from "@material-ui/icons/PostAddOutlined";
 
 function Feed() {
   return (
@@ -12,6 +17,20 @@ function Feed() {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption
+            Icon={PanoramaOutlinedIcon}
+            title="Photo"
+            color="#70B5F9"
+          />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#7FC15E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#E7A33E" />
+          <InputOption
+            Icon={PostAddOutlinedIcon}
+            title="Write Article"
+            color="#F5987E"
+          />
         </div>
       </div>
     </div>
