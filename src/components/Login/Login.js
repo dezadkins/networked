925d40d5229ff37769810c509f4924bed71a269a
Dez <dez@dezs-mbp.attlocal.net>
2021-03-16 13:05:16 -0400

    onchange handlers and value states

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import { auth } from "../../firebase";
 import "./Login.css";
 function Login() {
-  const register = () => {};
-  const loginToApp = () => {};
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = () => {
+    //   if()
+  };
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
       <img
@@ -11,16 +22,31 @@ function Login() {
       />
 
       <form>
-        <input placeholder="Full name (required if registering)" type="text" />
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full name (required if registering)"
+          type="text"
+        />
         <input placeholder="Profile pic URL (optional)" type="text" />
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          type="password"
+        />
         <button type="submit" onClick={loginToApp}>
           Sign In
         </button>
       </form>
       <p>
-        Not a member?
+        Not a member?{" "}
         <span className="login__register" onClick={register}>
           Register Now
         </span>
