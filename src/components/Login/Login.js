03397639707222153085b41b59e9c4147b16472c
Dez <dez@dezs-mbp.attlocal.net>
2021-03-16 12:57:17 -0400

    form setup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,30 @@
 import React from "react";
 import "./Login.css";
 function Login() {
+  const register = () => {};
+  const loginToApp = () => {};
   return (
     <div className="login">
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Ethernet.svg/1280px-Ethernet.svg.png"
         alt="ethernet logo"
       />
-      ,
+
       <form>
         <input placeholder="Full name (required if registering)" type="text" />
         <input placeholder="Profile pic URL (optional)" type="text" />
         <input placeholder="Email" type="email" />
         <input placeholder="Password" type="password" />
-        <button>Sign In</button>
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
       </form>
+      <p>
+        Not a member?
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
     </div>
   );
 }
